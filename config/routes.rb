c359c5b809a1fa042a1610a1992828395ff6c091
Julian Arango <julian_arango08@hotmail.com>
2017-03-19 18:30:13 -0500

    Implementando comentarios en cada articulo, hay un error

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  
-  resources :comments
+  resources :articles do
+    resources :comments,only: [:create,:destroy,:update]
+  end
   devise_for :users
-  resources :articles
-  
   root 'welcome#index'
-  get 'welcome/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
