a10cfb215c5e49d35a9c9a6e266e07f55a2356ca
Julian Arango <jarangol@eafit.edu.co>
2017-04-15 11:50:24 -0500

    Añadiendo covers a los articulos

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,8 @@ class Article < ApplicationRecord
     validates :body,  presence: true,length: { minimum: 20 }
     before_save :set_visits_count
 
+    has_attached_file :cover, styles: {medium: "1280x720", thumb: "800x600"}
+    validates_attachment_content_type :cover, content_type: /\Aimage\/.*\Z/
 
     def update_visits_count
       self.update(visits_count: self.visits_count + 1)
